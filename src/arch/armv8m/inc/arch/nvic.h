647aea528cde6cf42d2794d5544ce7ab6013d7e1
Daniel Oliveira <drawnpoetry@gmail.com>
2025-02-18 13:52:40 +0000

    feat(nvic): add driver functions
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/arch/armv8m/inc/arch/nvic.h
+++ src/arch/armv8m/inc/arch/nvic.h
@@ -7,60 +7,120 @@
 #define __NVIC_H__
 
 #include <bao.h>
-#include <platform.h>
-#include <vm.h>
+#include <arch/sysregs.h>
+#include <arch/systick.h>
 
-/* Nested Vectored Interrupt Controller */
-#define NVIC_BASE           (0xE000E100UL)
-#define NVIC_NS_BASE        (0xE002E100UL)
-#define NVIC                ((struct nvic*)NVIC_BASE)
-#define NVIC_NS             ((struct nvic*)NVIC_NS_BASE)
+typedef enum int_target { SECURE = 0, NONSECURE = 1 } int_target_t;
 
-#define NVIC_MAX_INTERRUPTS (496U)
-#ifndef PLAT_NVIC_MAX_INTERRUPTS
-#define PLAT_NVIC_MAX_INTERRUPTS NVIC_MAX_INTERRUPTS
-#endif
+static inline bool nvic_conflict(bitmap_t* interrupt_bitmap, irqid_t int_id)
+{
+    return bitmap_get(interrupt_bitmap, int_id);
+}
 
-#define NVIC_PRIO_BITS 3 // Number of priority bits implemented in the NVIC
-#ifndef PLAT_NVIC_PRIO_BITS
-#define PLAT_NVIC_PRIO_BITS NVIC_PRIO_BITS
-#endif
-struct nvic {
-    volatile uint32_t iser[16]; // 0x000 + x*4: Interrupt Set-Enable Register x
-    uint32_t reserved0[16];
-    volatile uint32_t icer[16]; // 0x080 + x*4: Interrupt Clear-Enable Register x
-    uint32_t reserved1[16];
-    volatile uint32_t ispr[16]; // 0x100 + x*4: Interrupt Set-Pending Register x
-    uint32_t reserved2[16];
-    volatile uint32_t icpr[16]; // 0x180 + x*4: Interrupt Clear-Pending Register x
-    uint32_t reserved3[16];
-    volatile uint32_t iabr[16]; // 0x200 + x*4: Interrupt Active Bit Register x
-    uint32_t reserved4[16];
-    volatile uint32_t itns[16]; // 0x280 + x*4: Interrupt Target Non-Secure Register x
-    uint32_t reserved5[16];
-    volatile uint32_t ipr[496]; // 0x300 + x*4: Interrupt Priority Register x
-    uint32_t reserved6[580];
-    volatile uint32_t stir;     // 0xe00: Software Trigger Interrupt Register
-};
+// TODO:ARMV8M - Dismantle into separate functions
+static inline void nvic_enable(struct nvic* ic, irqid_t int_id, bool en)
+{
+    if (en) {
+        ic->iser[int_id >> 5UL] |= (irqid_t)(1UL << (int_id & 0x1FUL));
+    } else {
+        ic->icer[int_id >> 5UL] |= (irqid_t)(1UL << (int_id & 0x1FUL));
+    }
+}
+
+static inline bool nvic_get_en(struct nvic* ic, irqid_t int_id)
+{
+    return ((ic->iser[int_id >> 5UL] & (irqid_t)(1UL << (int_id & 0x1FUL))) != 0UL) ? true : false;
+}
+
+static inline void systick_int_enable(struct systick* st, bool en)
+{
+    if (en) {
+        st->csr |= (irqid_t)SYSTICK_CSR_TICKINT;
+    } else {
+        st->csr &= ~(irqid_t)SYSTICK_CSR_TICKINT;
+    }
+}
+
+static inline bool nvic_get_pend(struct nvic* ic, irqid_t int_id)
+{
+    return ((ic->ispr[int_id >> 5UL] & (irqid_t)(1UL << (int_id & 0x1FUL))) != 0UL) ? true : false;
+}
+
+static inline bool systick_get_pend(struct systick* st)
+{
+    if (st == systick_ns) {
+        return ((scb_ns->icsr & SCB_ICSR_PENDSTSET) != 0UL) ? true : false;
+    } else {
+        return ((scb_s->icsr & SCB_ICSR_PENDSTSET) != 0UL) ? true : false;
+    }
+}
+
+static inline void nvic_set_pend(struct nvic* ic, irqid_t int_id)
+{
+    ic->ispr[int_id >> 5UL] |= (irqid_t)(1UL << (int_id & 0x1FUL));
+}
+
+static inline void systick_set_pend(struct systick* st)
+{
+    if (st == systick_ns) {
+        scb_ns->icsr |= SCB_ICSR_PENDSTSET;
+    } else {
+        scb_s->icsr |= SCB_ICSR_PENDSTSET;
+    }
+}
+
+static inline void nvic_clr_pend(struct nvic* ic, irqid_t int_id)
+{
+    ic->icpr[int_id >> 5UL] |= (irqid_t)(1UL << (int_id & 0x1FUL));
+}
+
+static inline void systick_clr_pend(struct systick* st)
+{
+    if (st == systick_ns) {
+        scb_ns->icsr |= SCB_ICSR_PENDSTCLR;
+    } else {
+        scb_s->icsr |= SCB_ICSR_PENDSTCLR;
+    }
+}
+
+static inline void nvic_int_target(int_target_t target, irqid_t int_id)
+{
+    if (!target) { // secure
+        nvic_s->itns[int_id >> 5UL] |= (irqid_t)(1UL << (int_id & 0x1FUL));
+    } else {       // non-secure
+        nvic_s->itns[int_id >> 5UL] &= ~(irqid_t)(1UL << (int_id & 0x1FUL));
+    }
+}
+
+static inline void nvic_set_prio(struct nvic* ic, irqid_t int_id, uint32_t prio)
+{
+    ic->ipr[int_id] = (uint8_t)((prio << (8U - NVIC_PRIO_BITS)) & (uint32_t)0xFFUL);
+}
+
+static inline void systick_set_prio(struct systick* st, uint32_t prio)
+{
+    if (st == systick_ns) {
+        scb_ns->shpr3 = ((prio << (8U - NVIC_PRIO_BITS)) & (uint32_t)0xFFUL) << 24;
+    } else {
+        scb_s->shpr3 = ((prio << (8U - NVIC_PRIO_BITS)) & (uint32_t)0xFFUL) << 24;
+    }
+}
+
+static inline uint32_t nvic_get_prio(struct nvic* ic, irqid_t int_id)
+{
+    return (ic->ipr[int_id] >> (8U - NVIC_PRIO_BITS));
+}
+
+static inline uint32_t systick_get_prio(struct systick* st)
+{
+    if (st == systick_ns) {
+        return ((scb_ns->shpr3 >> 24) >> (8U - NVIC_PRIO_BITS));
+    } else {
+        return ((scb_s->shpr3 >> 24) >> (8U - NVIC_PRIO_BITS));
+    }
+}
 
 void nvic_init(void);
-void nvic_cpu_init(void);
-void nvic_handle(void);
-void nvic_set_enbl(struct nvic* ic, irqid_t int_id, bool en);
-bool nvic_get_active(struct nvic* ic, irqid_t int_id);
-bool nvic_get_enbl(struct nvic* ic, irqid_t int_id);
-void nvic_set_prio(struct nvic* ic, irqid_t int_id, uint32_t prio);
-uint32_t nvic_get_prio(struct nvic* ic, irqid_t int_id);
-bool nvic_get_pend(struct nvic* ic, irqid_t int_id);
-void nvic_set_pend(struct nvic* ic, irqid_t int_id);
-void nvic_clr_pend(struct nvic* ic, irqid_t int_id);
-bool nvic_get_target(irqid_t int_id);
-void nvic_set_target(irqid_t int_id);
-void nvic_clear_target(irqid_t int_id);
-void nvic_send_ipi(cpuid_t target_cpu);
-bool nvic_id_valid(unsigned long id);
-
-struct vm;
-void nvic_assign_icu_to_vm(unsigned long id, struct vm* vm);
+void nvic_int_handle(void);
 
 #endif /* __NVIC_H__ */
--- src/arch/armv8m/nvic.c
+++ src/arch/armv8m/nvic.c
@@ -3,129 +3,16 @@
  * Copyright (c) Bao Project and Contributors. All rights reserved.
  */
 
-#include <arch/nvic.h>
-#include "arch/interrupts.h"
+#include <bao.h>
 #include <interrupts.h>
+
 #include <cpu.h>
-#include <fences.h>
-#include <platform_defs.h>
-#include <platform.h>
-
+#include <mem.h>
 #include <vm.h>
+#include <arch/nvic.h>
 
-BITMAP_ALLOC(valid, NVIC_MAX_INTERRUPTS);
-
-void nvic_init_ipi(void) { }
+extern irq_handler_t interrupt_handlers[MAX_INTERRUPT_HANDLERS];
 
 void nvic_init(void) { }
 
-void nvic_cpu_init(void)
-{
-    /* Nothing to do */
-}
-
-void nvic_set_enbl(struct nvic* ic, irqid_t int_id, bool en)
-{
-    if (en) {
-        ic->iser[((int_id) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)int_id) & 0x1FUL));
-    } else {
-        ic->icer[((int_id) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)int_id) & 0x1FUL));
-    }
-    DSB();
-    ISB();
-}
-
-bool nvic_get_enbl(struct nvic* ic, irqid_t int_id)
-{
-    return ((uint32_t)(((ic->iser[(((uint32_t)int_id) >> 5UL)] &
-                            (1UL << (((uint32_t)int_id) & 0x1FUL))) != 0UL) ?
-            true :
-            false));
-}
-
-void nvic_set_prio(struct nvic* ic, irqid_t int_id, uint32_t prio)
-{
-    ic->ipr[((uint32_t)int_id)] = (uint8_t)((prio << (8U - NVIC_PRIO_BITS)) & (uint32_t)0xFFUL);
-}
-
-uint32_t nvic_get_prio(struct nvic* ic, irqid_t int_id)
-{
-    return ((uint32_t)ic->ipr[((uint32_t)int_id)] >> (8U - NVIC_PRIO_BITS));
-}
-
-bool nvic_get_pend(struct nvic* ic, irqid_t int_id)
-{
-    return ((uint32_t)(((ic->ispr[(((uint32_t)int_id) >> 5UL)] &
-                            (1UL << (((uint32_t)int_id) & 0x1FUL))) != 0UL) ?
-            true :
-            false));
-}
-
-void nvic_set_pend(struct nvic* ic, irqid_t int_id)
-{
-    ic->ispr[(((uint32_t)int_id) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)int_id) & 0x1FUL));
-}
-
-void nvic_clr_pend(struct nvic* ic, irqid_t int_id)
-{
-    ic->icpr[(((uint32_t)int_id) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)int_id) & 0x1FUL));
-}
-
-bool nvic_get_target(irqid_t int_id)
-{
-    return ((uint32_t)(((NVIC->itns[(((uint32_t)int_id) >> 5UL)] &
-                            (1UL << (((uint32_t)int_id) & 0x1FUL))) != 0UL) ?
-            true :
-            false));
-}
-
-void nvic_set_target(irqid_t int_id)
-{
-    // Sets to NS
-    NVIC->itns[(((uint32_t)int_id) >> 5UL)] |= ((uint32_t)(1UL << (((uint32_t)int_id) & 0x1FUL)));
-}
-
-void nvic_clear_target(irqid_t int_id)
-{
-    // Clears to S
-    NVIC->itns[(((uint32_t)int_id) >> 5UL)] &= ~((uint32_t)(1UL << (((uint32_t)int_id) & 0x1FUL)));
-}
-
-bool nvic_get_active(struct nvic* ic, irqid_t int_id)
-{
-    return ((uint32_t)(((ic->iabr[(((uint32_t)int_id) >> 5UL)] &
-                            (1UL << (((uint32_t)int_id) & 0x1FUL))) != 0UL) ?
-            true :
-            false));
-}
-
-void nvic_handle(void) { }
-
-void nvic_send_ipi(cpuid_t target_cpu)
-{
-    UNUSED_ARG(target_cpu);
-}
-
-void nvic_config_irq(irqid_t int_id, bool en)
-{
-    UNUSED_ARG(int_id);
-    UNUSED_ARG(en);
-}
-
-void nvic_assign_int_to_vm(struct vm* vm, irqid_t id)
-{
-    UNUSED_ARG(vm);
-    UNUSED_ARG(id);
-}
-
-void nvic_assign_icu_to_vm(unsigned long id, struct vm* vm)
-{
-    UNUSED_ARG(id);
-    UNUSED_ARG(vm);
-}
-
-bool nvic_id_valid(unsigned long id)
-{
-    UNUSED_ARG(id);
-    return false;
-}
+void nvic_int_handle(void) { }
